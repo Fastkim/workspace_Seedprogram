926efa4779db37d12ff9abbef4f805628620e918
'dahunkim' <'ekgns019@gmail.com'>
2024-04-17 17:01:19 +0900

    2024-04-17

---
+++ python/7.kiwoom/3.stockCode.py
@@ -0,0 +1,43 @@
+from PyQt5.QAxContainer import QAxWidget
+from PyQt5.QtCore import QEventLoop
+from PyQt5.QtWidgets import QApplication
+import sys
+
+class kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+        self.regKey()
+        self.regHandlers()
+        self.login()
+    
+    def regKey(self):
+        self.setControl('KHOPENAPI.KHOpenAPICtrl.1')
+
+    def regHandlers(self):
+        self.OnEventConnect.connect(self.onLogin)
+
+    def onLogin(self, status):
+        result = '키움증권 연결 '
+        if status == 0: result += '성공'
+        else: result += '실패'
+        print(f'{result}했습니다.')
+        self.el.exit()
+
+    def login(self):
+        self.CommConnect()
+        self.el = QEventLoop()
+        self.el.exec()
+
+    def getAccNo(self):
+        accNo = self.GetLoginInfo('ACCNO').split(';')[0]
+        return accNo
+
+    def getStockCodes(self, marketType): # marketType : cospi, cosdaq
+        return self.GetCodeListByMarket(marketType).split(';')[:-1] # list의 마지막은 쓰레기이므로, 마지막 바로 전까지 [:-1]
+
+app = QApplication(sys.argv) # 부모창
+kiwoom = kiwoom()
+
+print(kiwoom.getStockCodes('0')) # 10번은 cosdaq, 0번이 정규장
+
+app.exec()
